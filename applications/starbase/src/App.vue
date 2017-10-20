8f44784065e20d17fc029e70cd818f2d98a65254
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-21 00:16:31 +0800

    Introducing Starwars API

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,6 +1,11 @@
 <template>
 <div id="app">
   <h3>{{ title }}</h3>
+  <button 
+    class="btn btn-primary"
+    @click="fetchCharacter">
+    Test Fetch Character
+  </button>
 </div>
 </template>
 
@@ -10,6 +15,14 @@ export default {
   data() {
     return {
       title: 'Generate Your Team'
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      const url = 
+      fetch('http://swapi.co/api/people/1', { method: 'GET' })
+        .then(response => response.json())
+        .then(json => console.log(json));
     }
   }
 }
