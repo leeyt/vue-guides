6d84463c4458fd01d1a8a304e05d205250b7a9f0
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-22 03:26:09 +0800

    Display multiple characters' data with looping

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,7 +1,13 @@
 <template>
 <div id="app">
   <h3>{{ title }}</h3>
-  <Character :id="1"/>
+  <div class="col-md-12">
+    <Character 
+      v-for="(id, index) in initial_ids"
+      :key="index"
+      :id="id"
+    />
+  </div>
 </div>
 </template>
 
@@ -12,7 +18,8 @@ export default {
   name: 'app',
   data() {
     return {
-      title: 'Generate Your Team'
+      title: 'Generate Your Team',
+      initial_ids: [ 1, 13, 14 ]
     }
   },
   components: {
