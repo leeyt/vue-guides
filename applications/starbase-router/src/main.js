4b2ca1963e5dcff4324f3c7251e3a4bb3be8470e
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-26 17:05:15 +0800

    Import VueRouter and define routes.

--- applications/starbase-router/src/main.js
+++ applications/starbase-router/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/data/:type' }
+]
 
 new Vue({
   el: '#app',
