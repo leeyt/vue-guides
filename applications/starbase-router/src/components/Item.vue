a98071afde6db2775842fb10a0c1b05939c91a45
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-27 18:17:45 +0800

    Update Item component to display item details.

--- applications/starbase-router/src/components/Item.vue
+++ applications/starbase-router/src/components/Item.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="col-md-4">
-    {{item}}
+    <div class="item-card">
+      <div class="card-block">
+        <h4 class="card-title">{{item.name}}</h4>
+        <div 
+          v-for="(value, key, index) in item"
+          :key="key">
+          <div v-if="index < 5">
+            <strong>{{key}}</strong>: {{value}}</strong>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
