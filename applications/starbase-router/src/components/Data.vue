502a7ab60856974af4ced3bc532135b45a80b751
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-27 17:58:05 +0800

    Update Data component to fetch items whenever route changed.

--- applications/starbase-router/src/components/Data.vue
+++ applications/starbase-router/src/components/Data.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="">
-    Type: {{type}}
+    {{items}}
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      items: []
+    }
+  },
   computed: {
     type() {
-      return this.$route.params.type
-    } 
+      return this.$route.params.type;
+    }
   },
+  methods: {
+    fetchItems() {
+      this.items = [];
+      let initial_ids = [1, 13, 14];
+
+      for (let i in initial_ids) {
+        let id = initial_ids[i];
+        fetch(`http://swapi.co/api/${this.type}/${id}`, { method: 'GET'})
+          .then(response => response.json())
+          .then(json => this.items.push(json));
+      }
+    }
+  },
+  watch: {
+    '$route': 'fetchItems'
+  },
+  created() {
+    this.fetchItems();
+  }
 }
 </script>
