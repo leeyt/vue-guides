eb3bb623e0ca72f16dbf8458b39970ba4cd745f0
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-29 16:13:33 +0800

    Implement 'sign out' function.

--- applications/turnout/src/store/actions.js
+++ applications/turnout/src/store/actions.js
@@ -3,3 +3,7 @@ import * as types from './mutation-types'
 export const signIn = ({commit}, user) => {
   commit(types.SIGN_IN, user)
 }
+
+export const signOut = ({commit}) => {
+  commit(types.SIGN_OUT)
+}
--- applications/turnout/src/store/mutations.js
+++ applications/turnout/src/store/mutations.js
@@ -3,5 +3,8 @@ import * as types from './mutation-types'
 export const mutations = {
   [types.SIGN_IN] (state, user) {
     state.user = user
+  },
+  [types.SIGN_OUT] (state) {
+    state.user = {}
   }
 }
