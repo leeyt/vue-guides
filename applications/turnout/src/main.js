a79db07c7ccb3cc14a54ca75f49d20083d29b6f0
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-29 13:59:43 +0800

    Scaffold components, and configure routing.

--- applications/turnout/src/main.js
+++ applications/turnout/src/main.js
@@ -1,7 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import Dashboard from './components/Dashboard.vue'
+import Signin from './components/Signin.vue'
+
+const routes = [
+  {
+    path: '/dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/signin',
+    component: Signin
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
