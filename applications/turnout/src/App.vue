b14c38215da1b0fa51e61ec291fed76deffb944a
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-29 15:43:30 +0800

    Update 'Signin' component to add a sign in form. Implement sign in functionality with firebase authentication.

--- applications/turnout/src/App.vue
+++ applications/turnout/src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <h4>App Component</h4>
-    <router-link to="/dashboard">Dashboard</router-link> |
-    <router-link to="/signin">Signin</router-link>
     <router-view />
   </div>
 </template>
